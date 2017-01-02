8cd186d3b8913bc1dc76c9de8b4087ed387ee8e8
Cody Taylor <codyalantaylor@gmail.com>
2017-01-02 09:08:52 -0600

    added Skeleton.css, added responsive formatting to index.php

--- index.php
+++ index.php
@@ -1,17 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Skeleton: Responsive CSS Boilerplate</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="stylesheet" type="text/css" href="normalize.css">
   <link rel="stylesheet" type="text/css" href="skeleton.css">
 </head>
 <body>
+  <div class="row">
+    <div class="twelve columns">
+      <h1>Markdown File Converter</h1>
+    </div>
+  </div>
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+    <div class="row">
+      <div class="twelve columns">
+        Select file to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload"></input>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns">
+        Output formats:
+        HTML <input type="checkbox" name="HTML" value="HTML"></input>
+        PDF <input type="checkbox" name="PDF" value="PDF"></input>
+        DOCX <input type="checkbox" name="DOCX" value="DOCX"></input>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns">
+        none <input type="radio" name="stylesheet" value="none" checked="checked"></input>
+        retro.css <input type="radio" name="stylesheet" value="retro"></input>
+        screen.css <input type="radio" name="stylesheet" value="screen"></input>
+        avenir-white.css <input type="radio" name="stylesheet" value="avenir-white"></input>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <p>Select file to upload: <input type="file" name="fileToUpload" id="fileToUpload"></p>
-    <p><input type="checkbox" name="HTML" value="HTML"> .html</input><input type="checkbox" name="PDF" value="PDF"> .pdf</input><input type="checkbox" name="DOCX" value="DOCX"> .docx</input></p>
-    <p><input type="radio" name="stylesheet" value="none" checked="checked">none</input><input type="radio" name="stylesheet" value="retro"> retro.css</input><input type="radio" name="stylesheet" value="screen"> screen.css</input><input type="radio" name="stylesheet" value="avenir-white"> avenir-white.css</input></p>
-    <p><input type="submit" value="Upload" name="submit"></input></p>
-</form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns">
+        <input type="submit" value="Upload" name="submit"></input>
+      </div>
+    </div>
+  </form>
 
 </body>
 </html>
