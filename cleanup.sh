6b70812d42e659ad181f074774b3992f729d668d
Cody Taylor <codyalantaylor@gmail.com>
2017-04-28 10:13:39 -0500

    added descriptions of what is taking place

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,8 @@
 # removes all files and directories except uploads/
+echo "moving into /var/www/html/ . . ."
+cd /var/www/html/
+echo "before clean-up:"
+ls
 rm -rf preview stylesheets *.css *.html *.md *.php *.sh
+echo "after clean-up:"
+ls
