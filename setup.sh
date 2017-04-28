dff659de8cef2f14825b461705f18c7dcaf3f93e
Cody Taylor <codyalantaylor@gmail.com>
2017-04-28 10:07:43 -0500

    no echos a description of what's happening

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # when ran from inside Pandoc-Web-Interface/ will copy all necessary files to root of web directory
+echo "copying tool to /var/www/html/ . . ."
 cp -r * /var/www/html/
 
 # copy PHP settings to correct path
+echo "copying php.ini to /etc/php/7.0/apache2/ . . ."
 sudo cp php.ini /etc/php/7.0/apache2/php.ini
