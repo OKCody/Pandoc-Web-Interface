5383de7f47af66728c22bd07f7bf6d86cfca3b1c
Cody Taylor <codyalantaylor@gmail.com>
2017-04-28 10:16:19 -0500

    cleaned up formatting

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 # removes all files and directories except uploads/
 echo "moving into /var/www/html/ . . ."
 cd /var/www/html/
+echo " "
 echo "before clean-up:"
 ls
 rm -rf preview stylesheets *.css *.html *.md *.php *.sh
+echo " "
 echo "after clean-up:"
 ls
