eca1d38493651414daab4db1179765b5bd933d7a
Cody Taylor <codyalantaylor@gmail.com>
2017-04-28 10:24:50 -0500

    added command to restart apache
    restarting apache is necessary to implement changes made to php.ini

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ cp -r * /var/www/html/
 # copy PHP settings to correct path
 echo "copying php.ini to /etc/php/7.0/apache2/ . . ."
 sudo cp php.ini /etc/php/7.0/apache2/php.ini
+
+# Restart Apache2
+echo "restarting apache . . ."
+sudo service apache2 restart
